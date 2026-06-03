2424aa55fe18cf47b7aebf4925e81708b715abce
bibekneupane <npne.bibek@gmail.com>
2026-06-03 13:50:40 +0545

    setup angular http todo service

---
+++ TodoApp.Client/src/app/services/todo.service.ts
@@ -0,0 +1,27 @@
+import {Injectable, inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Todo} from '../models/todo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  private http = inject(HttpClient);
+  private apiUrl = 'api/todo';
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+
+  deleteTodo(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  updateTodo(todo: Todo): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${todo.id}`, todo);
+  }
+}
