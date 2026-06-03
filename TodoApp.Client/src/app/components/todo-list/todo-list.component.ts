c27876046f35a350dc231eafd2398c508483d0aa
bibekneupane <npne.bibek@gmail.com>
2026-06-03 13:55:43 +0545

    todo-list component init

---
+++ TodoApp.Client/src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.css',
+})
+export class TodoListComponent implements OnInit {
+
+  ngOnInit() {
+  }
+}
