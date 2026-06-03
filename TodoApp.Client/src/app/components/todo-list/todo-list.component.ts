8ad33dc606034e9ef79d43bdb2831dacd435821e
bibekneupane <npne.bibek@gmail.com>
2026-06-03 17:10:23 +0545

    Load Todo App on home and remove template and fix proxy to correct port BE

--- TodoApp.Client/src/app/components/todo-list/todo-list.component.ts
+++ TodoApp.Client/src/app/components/todo-list/todo-list.component.ts
@@ -1,13 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal, inject} from '@angular/core';
+import {TodoService} from '../../services/todo.service';
+import {Todo} from '../../models/todo.model';
 
 @Component({
   selector: 'app-todo-list',
   imports: [],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.css',
+  styleUrl: './todo-list.component.css',
 })
 export class TodoListComponent implements OnInit {
+  private todoService = inject(TodoService);
+  todos = signal<Todo[]>([]);
 
   ngOnInit() {
+    this.todoService.getTodos().subscribe({
+      next: (data) => this.todos.set(data),
+      error: (err) => console.error('Failed to load todos list', err)
+    });
+  }
+
+  delete(id: string) {
+
   }
 }
